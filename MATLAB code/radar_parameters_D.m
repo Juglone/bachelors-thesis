42f3fa805fc68c0ab62c114a6c40f76fa330f116
tressell <tess-e@live.se>
2023-04-06 17:51:09 +0200

    clean up param files and add new

--- MATLAB code/radar_parameters_D.m
+++ MATLAB code/radar_parameters_D.m
@@ -1,10 +1,10 @@
-% == Radar parameters C ==
-% This file contains all radar parameters for configuration "C".
+% == Radar parameters D ==
+% This file contains all radar parameters for configuration "D".
 % To load in a script, simply write the name of this file
 
 num_samples = 96; % number of samples per chirp = ADC samples in mmWave Studio
-num_frames = 12000; % number of frames = number of chirps
-T_f = 10*1e-3; % frame periodicity in mmWave Studio
+num_frames = 4000; % number of frames = number of chirps
+T_f = 5*1e-3; % frame periodicity in mmWave Studio
 T_s = T_f/num_samples; % periodicity of chirp samples
 
 t_meas = num_frames*T_f; % length of radar meas in seconds
